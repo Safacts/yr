5709c23cfe0ce1a4ab9713b60a48813513373686
safacts <safacts001@gmail.com>
2024-03-10 15:46:52 +0530

    tried to solve the tuple error

--- yr_project/settings.py
+++ yr_project/settings.py
@@ -131,11 +131,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'        
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+'''
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+'''
+
+'''
+# This should be a list of directories where Django will look for static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# This is where static files will be collected to.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+'''
+'''
+<<<<<<<<<<<<
+
+STATIC_URL = '/staticfiles/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+                           
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+'''STATIC_URL = '/static/'  
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Specify a different directory for STATIC_ROOT
