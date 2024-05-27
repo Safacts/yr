82ae5a07e9cf8dc438eb61f88a3f5a5c156b829d
safacts <safacts001@gmail.com>
2024-05-27 11:54:29 +0530

    added page count feature

--- yr_app/views.py
+++ yr_app/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import PageView
+
 
 
 def homepage(request):
-    return render(request,"yrcorporation.html")
+    page_view, created = PageView.objects.get_or_create(pk=1)
+    page_view.count += 1
+    page_view.save()
+
+    context = {'view_count': page_view.count}
+    
+    # Debugging
+    print("PageView Count: ", page_view.count)
+    
+    return render(request, 'yrcorporation.html', context)
 
 def products(request):
     return render(request,"products.html")
